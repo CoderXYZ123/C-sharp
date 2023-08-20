52dcd1f527e45a31f6532ce44a90da956ce320de
CoderXYZ123 <87338585+CoderXYZ123@users.noreply.github.com>
2023-08-20 13:19:07 +0000

    Started with .Net

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World, welcome the world of .Net");
+
+//.Net - An open-source developer platform, created by Microsoft for building many different types of applications
+
